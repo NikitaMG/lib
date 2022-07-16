17a2b352a03dcceb99fd54485442874307bbe665
NikitaM123 <[nikitamalihin123@gmail.com]>
2022-07-16 11:19:05 +0300

    2

--- src/main/java/config/DatabaseConf.java
+++ src/main/java/config/DatabaseConf.java
@@ -1,4 +1,31 @@
 package config;
 
-public class DatabaseConf {
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConf implements Database{
+
+    private final String user = "nikita";
+    private final String password = "nikita123";
+    private final String url = "jdbc:postgresql://localhost:5432/ithillel.lesson22";
+    private final Connection connection;
+
+    public DatabaseConf() {
+        this.connection = createConnection();
+    }
+
+    private Connection createConnection() {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Connection getDBConnection() {
+        return this.connection;
+    }
 }
